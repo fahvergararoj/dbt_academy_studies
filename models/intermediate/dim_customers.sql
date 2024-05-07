1c7a751013a2d38fd792e49a4ba31c1a8cb9ba70
Fabian Humberto Vergara Rojas <fhvr@novonordisk.com>
2024-05-07 14:03:00 +0000

    Deploy

---
+++ models/intermediate/dim_customers.sql
@@ -0,0 +1,10 @@
+with
+    products as (
+        select * 
+        from {{ref('stg_northwind__customer')}}
+    )
+
+select 
+    row_number() over (order by id desc) as customer_sk
+    ,
+    from products
